61b86105293a4708ef7118b487a41adb4b99690f
phantom05 <monster2jy@gmail.com>
2020-06-23 12:25:30 +0900

    Update: parcel 세팅 완료

--- react-apollo-study2/src/Root.js
+++ react-apollo-study2/src/Root.js
@@ -3,8 +3,10 @@ import React from 'react';
 function Root(props) {
   const handleClick = config => {
     console.log('handelClick');
+    console.log(config, 'config');
+    config?.hello;
   };
-  return <div onClick={handleClick}>Root</div>;
+  return <div onClick={() => handleClick()}>Root</div>;
 }
 
 export default Root;
